90a210470027f11559f7c318e20e5b3ea62bdd65
mhafan <hrubym@me.com>
2024-03-28 10:02:58 +0100

    Initial Commit

---
+++ IZA-2024-cviko-2/IZA_2024_cviko_2App.swift
@@ -0,0 +1,17 @@
+//
+//  IZA_2024_cviko_2App.swift
+//  IZA-2024-cviko-2
+//
+//  Created by Martin Hruby on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct IZA_2024_cviko_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
